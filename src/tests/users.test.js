0f9200882dc40bb40df5f5e6454371f9f9c55528
MelVite <melanievite38@gmail.com>
2024-05-07 16:38:21 -0600

    Se modifican los archivos correspondientes para pruebas

--- src/tests/users.test.js
+++ src/tests/users.test.js
@@ -1,9 +1,19 @@
 // You already have the methods imported
-import { default as expect } from "expect";
-import { getUser, getUsers } from "../users";
+//import { default as expect } from "expect";
+//import { getUser, getUsers } from "../users";
+
+const users = require("..data/");
+const { getUsers, getUser } = require("../users");
 
 // Make a test to verify if the method getUser(id) works correctly
 // Verify any data of the resulting object you want
+
+describe('getUser', () => {
+    it('It should retrieve the correst user for a given ID', () => {
+        const user = user[0];
+        expect(getUser(1)).toEqual(user);
+    });
+});
 
 // READ documentation
 
